18941f31e165ed3c7f3463fd84db4be8194ac559
edcilo <edcilop@gmail.com>
2023-02-09 22:17:21 -0600

    feat(db): abstract engine class

--- app/db/engines/engine.py
+++ app/db/engines/engine.py
@@ -1,2 +1,32 @@
-class Engine():
-    pass
+from abc import ABC, abstractmethod, abstractproperty
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+class Engine(ABC):
+    def __init__(self):
+        self.base = declarative_base()
+
+    @abstractproperty
+    def url(self):
+        pass
+
+    def engine(self):
+        return create_engine(
+            self.url,
+            connect_args={"check_same_thread": False}
+        )
+
+    def session(self):
+        return sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=self.engine()
+        )
+
+    def create_all(self):
+        self.base.metadata.create_all(bind=self.engine())
+
+    def drop_all(self):
+        self.base.metadata.drop_all(bind=self.engine())
